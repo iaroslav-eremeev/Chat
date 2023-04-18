b9f9a6ec05639bc4ae7cd39440cb5f39c14eb4f3
Iaroslav <eremeev.pt@gmail.com>
2023-04-19 00:23:51 +0300

    Debug registration.js: change paths

---
+++ ChatServer/target/ChatServer-1.0-SNAPSHOT/scripts/registration.js
@@ -0,0 +1,27 @@
+$('#btn-go-login').click(function () {
+        $(location).attr('href', "http://localhost:8080/ChatServer/login.html");
+    }
+)
+
+
+$('#btn-sign-up').click(function () {
+        $.ajax({
+            url: 'registration',
+            method: "POST",
+            data: {"login" : $('#login').val(),
+                "password" : $('#password').val(),
+                "name" : $('#name').val(),},
+            success: [function (data) {
+                $('.popup-fade').fadeIn();
+            }],
+            error: [function (xhr, status, error) {
+                alert(xhr.responseText);
+            }]
+        })
+    }
+)
+
+$('#btn-ok').click(function () {
+    $('.popup-fade').fadeOut();
+    $(location).attr('href', "http://localhost:8080/ChatServer/login.html");
+})
